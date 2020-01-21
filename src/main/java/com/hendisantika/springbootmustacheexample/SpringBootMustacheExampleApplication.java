e39b217c7b2a19d80b131248803a159400221dd6
Hendi Santika <hendisantika@gmail.com>
2020-01-22 06:01:07 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootmustacheexample/SpringBootMustacheExampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootmustacheexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMustacheExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMustacheExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootmustacheexample/SpringBootMustacheExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootmustacheexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMustacheExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
